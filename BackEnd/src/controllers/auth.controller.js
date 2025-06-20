ea1deeb7eabbfd083fb02df0a8217f95e647c119
Abrar <ahmedabrararian@gmail.com>
2025-06-20 16:56:32 +0600

    Set Up Login, Logout & Profile Update Features along with protecting them with a JWT verification

--- BackEnd/src/controllers/auth.controller.js
+++ BackEnd/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -5,11 +6,10 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   const { fullName, email, phoneNumber, password } = req.body;
   try {
-
     if (!fullName || !email || password) {
       return res.status(400).json({ message: "Please fill required fields!" });
     }
-    
+
     if (password.length < 6) {
       return res
         .status(400)
@@ -44,15 +44,78 @@ export const signup = async (req, res) => {
       res.status(400).json({ message: "Invalid user data" });
     }
   } catch (error) {
-    console.log("Error in signup conrtoller", error.message);
+    console.log("Error in signup conrtoller:", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const isPassCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPassCorrect) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller:", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout route");
+  try {
+    res.cookie("jwt", "", { maxage: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findById(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in update profile controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,18 +1,20 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(express.json()); // to be able to get req data from frontend
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.listen(port,  () => {
-    console.log(`Server is running on port: ${port}`);
-    connectDB()
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+  connectDB();
+});
